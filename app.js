daf9db984ef8b9ae054be5248667d61314d2a7e2
Prem324 <prem324r@gmail.com>
2025-04-13 01:19:34 +0530

    Candidates-Mangement

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
 const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
