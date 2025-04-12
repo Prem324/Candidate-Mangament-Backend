de3af6b2d68749ff3637905a780fbb28125bfa49
Prem324 <prem324r@gmail.com>
2025-04-13 00:52:49 +0530

    Candidate-Mangement

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to Candidate Mangement Portal");
 });
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 app.use("/api/candidates", require("./routes/candidates"));
 
